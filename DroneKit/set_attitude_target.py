1860f086f6f83fe1da9b388010850e7c97fdd697
poncho <amanzano05@icloud.com>
2020-02-13 23:54:09 -0600

    No Gps test

--- DroneKit/set_attitude_target.py
+++ DroneKit/set_attitude_target.py
@@ -20,26 +20,12 @@ from pymavlink import mavutil # Needed for command message definitions
 import time
 import math
 
-# Set up option parsing to get connection string
-import argparse
-parser = argparse.ArgumentParser(description='Control Copter and send commands in GUIDED mode ')
-parser.add_argument('--connect',
-                   help="Vehicle connection target string. If not specified, SITL automatically started and used.")
-args = parser.parse_args()
 
-connection_string = args.connect
-sitl = None
-
-# Start SITL if no connection string specified
-if not connection_string:
-    import dronekit_sitl
-    sitl = dronekit_sitl.start_default()
-    connection_string = sitl.connection_string()
 
 
 # Connect to the Vehicle
-print('Connecting to vehicle on: %s' % connection_string)
-vehicle = connect(connection_string, wait_ready=True)
+print('Connecting to the Drone')
+vehicle = connect('/dev/ttyAMA0', wait_ready= True, baud=921600)
 
 def arm_and_takeoff_nogps(aTargetAltitude):
     """
